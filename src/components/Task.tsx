5e38186881e2619e4c6f61e3d4882bf035cc6cbd
fselvino <fernandoselvino@gmail.com>
2023-01-14 18:37:18 -0300

    checkbox redondo

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,22 @@
 
-import { PlusCircle,Trash} from 'phosphor-react'
+import { PlusCircle, Trash } from 'phosphor-react'
 import styles from './Task.module.css'
 
-export function Task(){
-    
+export function Task() {
+
 
     return (
         <li className={styles.contentList}>
-                            <div>         
-                                <input type="checkbox" className={styles.checkbox}/>
-                                <label>Minha primeira tarefa e fazer essa pagina funcionar</label>                                                       
-                            </div>  
-                            <Trash/>                          
-                        </li> 
+            <div>
+                <label className={styles.checkbox}>
+                    <input type="checkbox" />
+                </label>
+                <label>Minha primeira tarefa e fazer essa pagina funcionar</label>
+            </div>
+            <Trash />
+
+
+        </li>
     )
 
 }
